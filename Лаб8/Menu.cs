f2af4ab8f3f315f96898fafa054c587c57829608
Dima <dmitriy.chumachenko.00@mail.ru>
2023-12-02 00:37:41 +0300

    create

---
+++ Лаб8/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб8
+{
+    internal class Menu : MenuComponent
+    {
+        private List<MenuComponent> _menuComponents = new List<MenuComponent>();
+
+        public Menu(string name)
+            : base(name)
+        { }
+
+        public override void Add(MenuComponent component)
+        {
+            ArgumentNullException.ThrowIfNull(component);
+            _menuComponents.Add(component);
+        }
+
+        public override void Remove(MenuComponent component)
+        {
+            ArgumentNullException.ThrowIfNull(component);
+            _menuComponents.Remove(component);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine(name);
+            foreach (MenuComponent component in _menuComponents)
+            {
+                component.Print();
+            }
+        }
+    }
+}
---
+++ Лаб8/MenuComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб8
+{
+    internal abstract class MenuComponent
+    {
+        protected string name;
+
+        public MenuComponent(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Add(MenuComponent component) { }
+
+        public virtual void Remove(MenuComponent component) { }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
