3cd1076e3cff84311e21bbbc958676ffac0458ac
Dima <dmitriy.chumachenko.00@mail.ru>
2023-12-03 00:19:19 +0300

    Edited

---
+++ Лаб9/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб9
+{
+    internal class Discount:CoffeeDecorator
+    {
+        public Discount(Coffee coffee)
+        : base(coffee, (coffee.Price() * -0.07), "+скидка")
+        { }
+    }
+}
